06db37faad5c9547713fa645734fca92fc7ad851
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-03-07 17:51:05 +0600

    Added theme class to body tag

--- src/index.js
+++ src/index.js
@@ -6,17 +6,24 @@ import './index.css';
 
 import * as data from '../data/data.json';
 
+const THEMES = {
+  light: 'theme_light',
+  dark: 'theme_dark',
+};
+
 window.renderTemplate = renderTemplate;
 
 window.onload = async function () {
   const params = new URLSearchParams(window.location.search);
   const slide = params.get('slide') || 1;
-  const theme = params.get('theme') || 'light';
+  const theme = params.get('theme') || 'dark';
   console.log(`slide: ${slide}; theme: ${theme}`);
   console.log(data[slide - 1]);
 
+  const root = document.getElementById('root');
+  root.classList.add(THEMES[theme] || THEMES.dark);
+
   const slideData = data[slide - 1];
   const slideRendered = await window.renderTemplate(slideData.alias, slideData.data);
-  const root = document.getElementById('root');
   root.insertAdjacentHTML('beforeend', slideRendered);
 }
