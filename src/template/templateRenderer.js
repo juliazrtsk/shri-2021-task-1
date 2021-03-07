50d51425aa9f4ff9cc72cf87e7f39fb518ed6072
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-03-07 18:19:01 +0600

    Made slide render function getting from map by alias

--- src/template/templateRenderer.js
+++ src/template/templateRenderer.js
@@ -1,10 +1,31 @@
 import leadersSlide from '../slides/leaders/leadersSlide';
+import voteSlide from '../slides/vote/voteSlide';
 
 import './style.css';
 
+const aliases = {
+  leaders: 'leaders',
+  vote: 'vote',
+  chart: 'chart',
+  diagram: 'diagram',
+  activity: 'activity',
+};
+
+const slideMap = {
+  [aliases.leaders]: (data) => leadersSlide(data),
+  [aliases.vote]: (data) => voteSlide(data),
+  [aliases.chart]: (data) => {},
+  [aliases.diagram]: (data) => {},
+  [aliases.activity]: (data) => {},
+};
+
 export async function renderTemplate (alias, data) {
-  /* Todo: add selection of render function depending on alias */
-  const renderedContent = await leadersSlide(data);
+  if (!aliases[alias]) {
+    console.error('Not found');
+    return;
+  }
+  const renderSlide = slideMap[alias];
+  const renderedContent = await renderSlide(data);
   return (
     `<div class="slide">
        <div class="slide__header">
